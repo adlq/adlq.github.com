c13709ec44df9fb285adffc8af9ed2dd320e2c9a
adlq <dtnghia90@gmail.com>
2013-03-23 23:19:02 +0100

    Test POST data receiving in Python

--- makaw.py
+++ makaw.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
-import sys
-import os
-import tweepy
+import web
 
-consumerKey = 'myaYupXCSINvlAuwScIa3w'
-consumerSecret = '97idfD3sWNbpowOVs3ZiHp83bFT5vZbdHNxItMUu4'
+urls = ('/.*', 'hooks')
 
-accessToken = '163395512-MLcrxHBjYwBK3oYBa4SDFYLIabJyEWkB1LZMCRy2'
-accessSecret = 'y5ZLf5cqAldsKSeDR037KnC0MQ8MpoifDce83X3oHI'
+app = web.application(urls, globals())
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessSecret)
-api = tweepy.API(auth)
+class hooks:
+    def POST(self):
+        data = web.data()
+        print
+        print 'DATA RECEIVED:'
+        print data
+        print
 
-os.system("touch hello.md")
-#api.update_status(sys.argv[1])
+if __name__ == '__main__':
+    app.run()
